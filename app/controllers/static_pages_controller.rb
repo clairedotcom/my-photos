598803c4bbb621b371886c7c9099b1422e788d9c
clairedotcom <claireacarson@gmail.com>
2022-03-09 12:09:29 -0500

    Add form to static_pages#index view and write index action in StaticPagesController

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
+  require 'flickr'
+  
   def index
+    flickr = Flickr.new(ENV['FLICKR_API_KEY'], ENV['FLICKR_SHARED_SECRET'])
+
+    if params.has_key?(:commit)
+      @photos = flickr.people.getPhotos(api_key: ENV['FLICKR_API_KEY'], user_id: params[:user_id]) 
+    end
   end
 end
